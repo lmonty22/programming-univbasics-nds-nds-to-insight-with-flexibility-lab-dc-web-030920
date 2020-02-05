f59c1b1fa4c50f80847c11ddb227e0621fde7661
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-05 16:18:18 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,6 +32,7 @@ end
 
 
 # Your code after this point
+pp directors_database
 
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
@@ -48,6 +49,13 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+
+movie_index = 0 
+  while movie_index < movies_collection.length do 
+    movies_collection[movie_index][:director_name] = name
+    movie_index += 1
+  end
+  movies_collection
 end
 
 
@@ -63,9 +71,54 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+ studio_grosses = {}
+ index = 0 
+    while index < collection.length do 
+      studio = collection[index][:studio]
+      movie_gross = collection[index][:worldwide_gross]
+    if !studio_grosses[studio]
+      studio_grosses[studio] = movie_gross
+    else 
+      studio_grosses[studio] += movie_gross
+    end
+    index += 1 
+  end
+  studio_grosses
 end
 
+#def summary_snack_count_by_prices(snacks)
+ # result = {}
+  #i = 0
+ 
+  #while i < snacks.length do
+    # For readability, let's save this lookup to somethign meaningful
+   # snack_name = snacks[i][:name]
+    #snack_price = snacks[i][:price]
+    # If there's no key for this number, add the number as a key and assign it
+    # a new Array for holding future snacks with that price
+    #if !result[snack_price]
+     # result[snack_price] = 1
+  #  else
+   #   result[snack_price] += 1
+    #end
+    #i += 1
+  #end
+ 
+  #result
+#end
+
+
 def movies_with_directors_set(source)
+  director_index = 0 
+  movie_arrays = []
+  while director_index < source.length do 
+    dname = source[director_index][:name]
+    director_movie_array = source[director_index][:movies]
+    movies_with_dname = movies_with_director_key(dname, director_movie_array)
+    movie_arrays.push(movies_with_dname)
+    director_index += 1
+  end
+  movie_arrays
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
